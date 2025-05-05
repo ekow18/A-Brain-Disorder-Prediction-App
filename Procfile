2ac07df3790f060e92361711e5a027faabdf29d2
ekow18 <ekowyankson18@gmail.com>
2025-05-05 19:44:12 +0000

    3rd commit with Profile for the render app and the requirements.txt file

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn conversational_input:app
--- conversational_input.py
+++ conversational_input.py
@@ -27,4 +27,4 @@ def chat():
     return render_template('chat.html', history=session['history'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
